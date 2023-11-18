4cd868207ba6b476be53d612a09db141023435be
OskarMihalik <45460241+OskarMihalik@users.noreply.github.com>
2023-11-18 23:11:02 +0100

    feat: typed sql macro, getting all todos

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
-use axum::{routing::get, Json, Router};
+use axum::extract::State;
+use axum::{routing::get, Router};
 use maud::{html, Markup, DOCTYPE};
-use serde::{Deserialize, Serialize};
+use sqlx::sqlite::SqlitePoolOptions;
+use sqlx::{migrate::MigrateDatabase, Sqlite};
+use sqlx::{Pool, SqlitePool};
+
+const DB_URL: &str = "sqlite://sqlite.db";
 
 #[tokio::main]
 async fn main() {
+    // db connection
+    if !Sqlite::database_exists(DB_URL).await.unwrap_or(false) {
+        println!("Creating database {}", DB_URL);
+        match Sqlite::create_database(DB_URL).await {
+            Ok(_) => println!("Create db success"),
+            Err(error) => panic!("error: {}", error),
+        }
+    } else {
+        println!("Database already exists");
+    }
+    let db_pool = SqlitePoolOptions::new().connect(DB_URL).await.unwrap();
+
+    setup_db(&db_pool).await;
+    // let pool = SqlitePoolOptions::new().max_connections(5).connect("/db/d")
+
     // build our application with a single route
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/json", get(json_handler))
+        .route("/todos", get(get_todos).post(create_new_todo))
         .route("/html", get(head))
-        .route("/htmx/getSomething", get(item))
+        .with_state(db_pool)
         .nest("/static", axum_static::static_router("static"));
 
     // run it with hyper on localhost:3000
@@ -19,16 +39,52 @@ async fn main() {
         .unwrap();
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Ass {
-    ass: String,
+async fn get_todos(State(db_pool): State<SqlitePool>) -> Markup {
+    let result = sqlx::query!("SELECT name FROM todo")
+        .fetch_all(&db_pool)
+        .await;
+    // println!("result: {:?}", result)
+    match result {
+        Ok(todos) => {
+            // todo.into().
+            // todo.name;
+            // todos.iter().map(|todo| todo.name).collect();
+            let new_todo: Vec<&str> = todos.iter().map(|record| record.name.as_str()).collect();
+            return todos_items(new_todo);
+        }
+        Err(error) => return error_response(&error.to_string()),
+    }
 }
 
-async fn json_handler() -> Json<Ass> {
-    let ass = Ass {
-        ass: "asdfasfd".to_owned(),
-    };
-    Json(ass)
+async fn create_new_todo(State(db_pool): State<SqlitePool>) -> Markup {
+    let result = sqlx::query!("--sql INSERT INTO todo (name) values ($1)")
+        .execute(&db_pool)
+        .await;
+    match result {
+        Ok(_) => return ok_response(),
+        Err(error) => return error_response(&error.to_string()),
+    }
+}
+
+async fn setup_db(db: &Pool<Sqlite>) {
+    let result = sqlx::query("CREATE TABLE IF NOT EXISTS todo (id INTEGER PRIMARY KEY NOT NULL, name VARCHAR(250) NOT NULL);").execute(db).await.unwrap();
+    println!("Create user table result: {:?}", result);
+}
+
+fn error_response(error: &str) -> Markup {
+    html!(
+        div {
+            (error)
+        }
+    )
+}
+
+fn ok_response() -> Markup {
+    html!(
+        div {
+            "ok"
+        }
+    )
 }
 
 async fn head() -> Markup {
@@ -44,24 +100,29 @@ async fn head() -> Markup {
                 script src="https://unpkg.com/htmx.org@1.9.8" integrity="sha384-rgjA7mptc2ETQqXoYC3/zJvkU7K/aP44Y+z7xQuJiVnB/422P/Ak+F/AqFR7E4Wr" crossorigin="anonymous" {};
             }
             body
-                hx-get="/htmx/getSomething"
-                hx-target="#getHere"
-                ."text-red-600" {
-                button  {
+                ."flex justify-center flex-col items-center" {
+                button
+                    hx-get="/todos"
+                    hx-target="#todos"
+                    ."border-black border max-w-xs" {
                     "Click me please"
                 }
-                "asdfasf"
-                div id="getHere" {
+                p ."text-center" {
+                    "asdfasf"
+                }
+                div id="todos" {
 
                 }
             }
     }
 }
 
-async fn item() -> Markup {
+fn todos_items(todos: Vec<&str>) -> Markup {
     html!(
-        li {
-            "fffffffffffff"
+        @for todo in &todos{
+            li {
+                (todo)
+            }
         }
     )
 }
